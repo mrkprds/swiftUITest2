8266078fa1ce3322fbaf519fa39476cec7261d56
Mark Patrick Perdon <markpatrickperdon@gmail.com>
2021-10-01 19:50:55 +0800

    Initial Commit

---
+++ SwiftUITest2/SwiftUITest2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITest2App.swift
+//  SwiftUITest2
+//
+//  Created by markpatrick on 10/1/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITest2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
